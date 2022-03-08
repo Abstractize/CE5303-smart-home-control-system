25cd2b595cbc15b79281ff46fcfbadf1ba5d41f5
Abstractize <gababarca@hotmail.com>
2022-03-07 22:22:45 -0600

    [add] Database Pin Values

--- Server/Data/Models/Door.cs
+++ Server/Data/Models/Door.cs
@@ -5,6 +5,7 @@ namespace Data.Models
     [Table("Door")]
     public class Door : Model
     {
+        public int Pin { get; set; }
         [ForeignKey("Room")]
         public Guid RoomId { get; set; }
         public Room Room { get; set; }
---
+++ Server/Data/Models/Light.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Models
+{
+    [Table("Light")]
+    public class Light : Model
+    {
+        public int Pin { get; set; }
+        [ForeignKey("Room")]
+        public Guid RoomId { get; set; }
+        public Room Room { get; set; }
+    }
+}
