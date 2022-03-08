168850d7ea4489056aa724f692d064aa55fd23ba
esteban alvarado <estalvgs1999@gmail.com>
2022-03-08 01:47:05 -0600

    [add] import dll function in hardware service

--- Server/Services/Implementation/HardwareService.cs
+++ Server/Services/Implementation/HardwareService.cs
@@ -6,21 +6,27 @@ namespace Services.Implementation
     public class HardwareService : IHardwareService
     {
         [DllImport("libHardwareController")]
-        static extern IntPtr GetHello();
+        static extern int enablePin(int pin);
         [DllImport("libHardwareController")]
-        static extern int PrintHello();
+        static extern int disablePin(int pin);
+        [DllImport("libHardwareController")]
+        static extern int pinMode(int pin, string mode);
+        [DllImport("libHardwareController")]
+        static extern int digitalWrite(int pin, int value);
+        [DllImport("libHardwareController")]
+        static extern int digitalRead(int pin);
 
-        public Task<String?> GetHelloAsync()
+        public Task<String?> setPin(int pin)
         {
-            IntPtr helloPtr = GetHello();
-            String? hello = Marshal.PtrToStringAnsi(helloPtr);
-            return Task.FromResult(hello);
+            // IntPtr helloPtr = GetHello();
+            // String? hello = Marshal.PtrToStringAnsi(helloPtr);
+            // return Task.FromResult(hello);
         }
 
         public Task SayHello()
         {
-            int hello = PrintHello();
-            return Task.FromResult(hello);
+            // int hello = PrintHello();
+            // return Task.FromResult(hello);
         }
     }
 }
