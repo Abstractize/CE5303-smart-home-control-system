035125abaed1ee9bda4131c908827095e259df80
estalvgs <estalvgs1999@estudiantec.cr>
2022-03-28 23:48:04 -0600

    [add] enabiling pin routine

--- Server/API/run.sh
+++ Server/API/run.sh
@@ -1,3 +1,11 @@
-export SYS_PATH="/sys/class/gpio"
-chmod 777 ./API
-./API
+
+# Enable all pins before initialize
+echo "--> Exporting all GPIO pins ..."
+for pin in 2 3 4 17 27 22 10 9 11
+do
+  echo $pin >> /sys/class/gpio/export
+done
+
+# Run
+echo "--> Running API ..."
+${dotnet} API.dll
